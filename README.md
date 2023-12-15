e050a1b567855acb11d910eabc38d538eb1174af
Bryan M Lenihan <blenihan@tiberhealth.com>
2023-12-15 10:27:54 -0500

    Tiber Logger initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tiber Logger for Tiber Health Innovations and Partners
+Tiber Logger is a common logging routine used by Tiber Health Python projects to produce consistent and common logging between our scripts and applications. 
+
+This tool was written and tested using Python 3.11. 
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tiber-logger',
+    use_scm_version=True,  # Enable setuptools_scm for versioning
+    setup_requires=['setuptools_scm'],  # Add setuptools_scm as a setup requirement
+    packages=find_packages(),
+    url='https://github.com/tiberhealth/TiberLogger',
+    license='',
+    author='Tiber Health Innovations',
+    author_email='tiber-tools@tiberhealth.com',
+    description='Common logging routine for Tiber Health Python  scripts and appplications',
+    long_description='Tiber Logger is a common Python package to assist with a common logging routine for Tiber Health scripts and applications',
+    install_requires=[]
+)
